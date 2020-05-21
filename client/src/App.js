55b5035e3a999115243bff302914ad0a31b976fd
alba <albaponslloret@gmail.com>
2020-05-21 10:56:21 +0200

    dijous 1

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      Hello World!
+      <i className="fab fa-fly"></i>
+      <i className="fas fa-feather-alt"></i>
+      <i className="fas fa-map-marker fa-rotate-180"></i>
+      <i className="fas fa-egg"></i>
+      <i className="fas fa-burn fa-5x"></i>
+      <i className="fas fa-user-plus"></i>
+      <i className="fas fa-plus-circle"></i>
+    </div>
+  );
+}
+
+export default App;
