22e744615030f75c23538390c1bf30ee06ab2055
alba <albaponslloret@gmail.com>
2020-05-25 15:56:12 +0200

    dilluns

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      Hello World!
-      <i className="fab fa-fly"></i>
-      <i className="fas fa-feather-alt"></i>
-      <i className="fas fa-map-marker fa-rotate-180"></i>
-      <i className="fas fa-egg"></i>
-      <i className="fas fa-burn fa-5x"></i>
-      <i className="fas fa-user-plus"></i>
-      <i className="fas fa-plus-circle"></i>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allFamily: null,
+    };
+  }
+
+  componentDidMount() {
+    this.getFamily();
+  }
+
+  getFamily = () => {
+    fetch(`/family`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        this.setState({ allFamily: response });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  deletePerson = (id) => {
+    fetch(`/students/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({ allFamily: response });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.allFamily.map((e, index) => (
+            <li key={index}>
+              <span>
+                {e.firstname} {e.lastname}
+              </span>
+              <button
+                onClick={() => this.deletePerson(e.id)}
+                className=" mx-2btn btn-sm btn-outline-danger"
+              >
+                <i className="far fa-trash-alt"></i>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
--- routes/family.js
+++ routes/family.js
@@ -1,12 +1,7 @@
 var express = require("express");
 var router = express.Router();
-
-/* GET family listing. */
-// router.get("/", function (req, res, next) {
-//   res.send("respond with a resource");
-// });
-
-// /* GET the family tree. */
+// suuuuuper important!
+const db = require("../model/helper");
 
 /* GET a list of family members. */
 router.get("/", function (req, res, next) {
@@ -17,39 +12,97 @@ router.get("/", function (req, res, next) {
     .catch((err) => res.status(500).send(err));
 });
 
-// function getFamily(req, res) {
-//   db(`SELECT * FROM people;`)
+function getFamily(req, res) {
+  db(`SELECT * FROM people;`)
+    .then((results) => {
+      res.send(results.data);
+    })
+    .catch((err) => res.status(500).send(err));
+}
+router.get("/", getFamily);
+
+/* GET the parents table. */
+// router.get("/parents", function (req, res, next) {
+//   db(`SELECT * FROM parents;`)
 //     .then((results) => {
 //       res.send(results.data);
 //     })
 //     .catch((err) => res.status(500).send(err));
-// }
-// router.get("/", getFamily);
-
-// // /* INSERT a new relative into the DB */
-// router.post("/", function (req, res, next) {
-//   const { firstName, lastName, partner } = req.body;
-//   db(
-//     `INSERT INTO people (firstName, lastName) VALUES ("${firstName}", "${lastName}", "${partner}")`
-//   )
-//     .then(() => {
-//       getStudents(req, res);
-//     })
-//     .catch((err) => res.status(500).send(err));
 // });
 
-// /* INSERT new partner relationship */
-// router.post("/", function (req, res, next) {
-//   const { firstName, lastName, partner } = req.body;
-//   db(
-//     `INSERT INTO parent (firstName, lastName) VALUES ("${firstName}", "${lastName}", "${partner}")`
-//   )
-//     .then(() => {
-//       getStudents(req, res);
-//     })
-//     .catch((err) => res.status(500).send(err));
-// });
+// /* No sé si fa falta */
+// // function getParents(req, res) {
+// //   db(`SELECT * FROM parents;`)
+// //     .then((results) => {
+// //       res.send(results.data);
+// //     })
+// //     .catch((err) => res.status(500).send(err));
+// // }
+// // router.get("/parents", getParents);
+
+/* GET the family tree from 1 person. */
+router.get("/:id", function (req, res, next) {
+  const { id } = req.params;
+
+  db(`SELECT * FROM people;`)
+    .then((results) => {
+      res.send(results.data);
+    })
+    .catch((err) => res.status(500).send(err));
+});
 
 /* INSERT a new relative into the DB */
+router.post("/", function (req, res, next) {
+  const { firstName, lastName } = req.body;
+  db(
+    `INSERT INTO people (firstName, lastName) VALUES ("${firstName}", "${lastName}");`
+  )
+    .then(() => {
+      getFamily(req, res);
+    })
+    .catch((err) => res.status(500).send(err));
+});
 
+/* INSERT new partner relationship */
+router.post("/:id", function (req, res, next) {
+  const { id } = req.params;
+  const { progenitor_1, progenitor_2 } = req.body;
+  let couple = null;
+
+  db(
+    `INSERT INTO parents (progenitor_1, progenitor_2) VALUES ("${progenitor_1}", "${progenitor_2}");`
+  )
+    .then((results) => {
+      couple = results.data[0];
+      console.log(couple);
+      // db(`UPDATE people SET couple_id = ${couple.}, WHERE id = ${id};`)
+      //   .then((results) => {
+      //     res.send(results.data);
+      //   })
+      //   .catch((err) => res.status(500).send(err));
+    })
+    .catch((err) => res.status(500).send(err));
+
+  next();
+});
+
+/* DELETE one person */
+router.delete("/:id", function (req, res, next) {
+  const { id } = req.params;
+  let person = null;
+
+  db(`SELECT * FROM people WHERE id = ${id}`)
+    .then((results) => {
+      person = results.data[0];
+
+      db(`DELETE FROM people WHERE id = ${id};`)
+        .then((results) => {
+          res.send({
+            msg: `${person.firstName} ${person.lastName} was deleted!`,
+          });
+        })
+        .catch((err) => res.status(500).send(err));
+    })
+    .catch((err) => res.status(500).send(err));
+});
 module.exports = router;
